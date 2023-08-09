5060b7976b3b40bcbc53dd7df44303e3cb1d851f
Dennis Heltzel <dheltzel@comspoc.com>
2023-08-09 14:50:23 -0400

    Added additional grants

--- deploy_scripts/create_schema.sql
+++ deploy_scripts/create_schema.sql
@@ -5,3 +5,10 @@
 create user &&1 identified by "&&2";
 grant connect,resource to &&1;
 grant unlimited tablespace to &&1;
+
+grant select on SYS.DBA_SCHEDULER_JOBS to &&1;
+grant select on SYS.DBA_TAB_PARTITIONS to &&1;
+grant select on SYS.DBA_TAB_SUBPARTITIONS to &&1;
+grant select on SYS.DBA_PARALLEL_EXECUTE_TASKS to &&1;
+grant select on SYS.DBA_PARALLEL_EXECUTE_CHUNKS to &&1;
+
